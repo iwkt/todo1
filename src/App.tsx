5019ebe96a98bc3b3eaa602fc3aa55093a84237d
iwkt <iwk@mac.com>
2022-07-09 06:44:18 +0900

    theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Button, Loader, MantineProvider, Paper, Text } from '@mantine/core';
+
 import { Todo } from './components/Todo';
 import { DemoButton } from './components/Hoge';
 import './App.css';
@@ -7,6 +9,21 @@ function App() {
   return (
     <div className="App">
       <h1 className="p-10 text-6xl text-blue-500">Hello Japan</h1>
+      <MantineProvider
+        theme={{
+          fontFamily: 'Open Sans',
+          colorScheme: 'dark',
+          fontSizes: { md: 60 },
+        }}
+      >
+        <Paper>
+          <Text>This is a simple text Tag</Text>
+          <Text>This is a simple text Tagajhfaoe opje</Text>
+        </Paper>
+        <Button>Hello World</Button>
+        <Button>Hello World</Button>
+        <Loader></Loader>
+      </MantineProvider>
       <Todo />
       <DemoButton />
     </div>

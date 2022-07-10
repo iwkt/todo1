4e925629b97bd64b71e40d6ad9de720173ddd5ed
iwkt <iwk@mac.com>
2022-07-11 08:59:47 +0900

    202207110900

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Space, Button, Loader, MantineProvider, Paper, Text, ThemeIcon } from '@mantine/core';
+import { Space, Button, Loader, Paper, Text } from '@mantine/core';
 
 import { Todo } from './components/Todo';
 import { DemoButton } from './components/Hoge';
@@ -9,27 +9,13 @@ function App() {
   return (
     <div className="App">
       <h1 className="p-10 text-6xl text-blue-500">Hello Japan</h1>
-      <MantineProvider
-        theme={{
-          fontFamily: 'Open Sans',
-          colorScheme: 'dark',
-          fontSizes: { md: 60 },
-          colors: {
-            pink: [
-            "#FF6B6B",
-            "#22B8CF"
-          ]
-          },
-        }}
-      >
-        <Paper>
-          <Text>This is a simple text Tag</Text>
-          <Text>This is a simple text Tagajhfaoe opje</Text>
-          <Button className="text-pink-200">Hello World</Button>
-          <Button>Hello World</Button>
-        </Paper>
-        <Loader></Loader>
-      </MantineProvider>
+      <Paper>
+        <Text>This is a simple text Tag</Text>
+        <Text>This is a simple text Tagajhfaoe opje</Text>
+        <Button className="text-pink-200">Hello World</Button>
+        <Button>Hello World</Button>
+      </Paper>
+      <Loader></Loader>
       <Todo />
       <DemoButton />
       <Text>First line</Text>
--- src/components/Hoge.tsx
+++ src/components/Hoge.tsx
@@ -1,20 +1,14 @@
-import { Button, Code, Title, MantineProvider } from '@mantine/core';
+import { Button, Code, Title } from '@mantine/core';
 
 export const DemoButton = () => {
   const clickAlert = () => {
     alert('今日もいい日に決まっています！');
   };
   return (
-    <MantineProvider
-      theme={{
-        fontFamily: 'Verdana, sans-serif',
-        fontFamilyMonospace: 'Monaco, Courier, monospace',
-        headings: { fontFamily: 'Greycliff CF, sans-serif' },
-      }}
-    >
+    <>
       <Title order={3}>Greycliff CF or sans-serif title</Title>
       <Button onClick={clickAlert}>Verdana button</Button>
       <Code>Monaco, Courier Code</Code>
-    </MantineProvider>
+    </>
   );
 };
